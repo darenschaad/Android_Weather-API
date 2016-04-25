c7d621e99b850e30397320036e471b04cc07db79
Daren Schaad and Polina Nenchev <polina.mihova@yahoo.com>
2016-04-25 16:46:29 -0700

    set up service to grab the name from the API call

--- app/src/main/java/com/epicodus/weatherapplication/models/Weather.java
+++ app/src/main/java/com/epicodus/weatherapplication/models/Weather.java
@@ -4,22 +4,22 @@ package com.epicodus.weatherapplication.models;
  * Created by Guest on 4/25/16.
  */
 public class Weather {
-    private String mLocation;
+    private String mName;
     private String mDateTime;
     private String mDayTemp;
     private String mDayTempLow;
     private String mDayTempHigh;
 
-    public Weather(String location, String dateTime, String dayTemp, String dayTempLow, String dayTempHigh) {
-        this.mLocation = location;
+    public Weather(String name, String dateTime, String dayTemp, String dayTempLow, String dayTempHigh) {
+        this.mName = name;
         this.mDateTime = dateTime;
         this.mDayTemp = dayTemp;
         this.mDayTempLow = dayTempLow;
         this.mDayTempHigh = dayTempHigh;
     }
 
-    public String getLocation() {
-        return mLocation;
+    public String getName() {
+        return mName;
     }
 
     public String getDateTime() {
--- app/src/main/java/com/epicodus/weatherapplication/services/WeatherService.java
+++ app/src/main/java/com/epicodus/weatherapplication/services/WeatherService.java
@@ -3,12 +3,21 @@ package com.epicodus.weatherapplication.services;
 import android.util.Log;
 
 import com.epicodus.weatherapplication.Constants;
+import com.epicodus.weatherapplication.models.Weather;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
@@ -35,5 +44,26 @@ public class WeatherService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    public ArrayList<Weather> processResults(Response response) {
+        ArrayList<Weather> weathers = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject openweatherJSON = new JSONObject(jsonData);
+                JSONArray weathersJSON = openweatherJSON.getJSONArray("weathers");
+                for (int i = 0; i < weathersJSON.length(); i++) {
+                    JSONObject weatherJSON = weathersJSON.getJSONObject(i);
+                    String name = weatherJSON.getString("name");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return weathers;
     }
 }
